586ada79ebe6aaa02388b92f89f23d798ab2c19f
Yenrou16 <114212393+Yenrou16@users.noreply.github.com>
2023-05-13 15:54:39 +0800

    Update Customer.java

--- OOPAssignment/src/Customer.java
+++ OOPAssignment/src/Customer.java
@@ -1,68 +1,101 @@
-//public class Customer {
-//    private String custId;
-//    private static String custName;
-//    private int custIc;
-//    private String contact;
-//    private String email;
-//    private int balance;
-//
-//    public Customer(){
-//    }
-//
-//    public Customer(String custId,String custName, int custIc, String contact, String email, int balance){
-//        this.custId = custId;
-//        this.custName = custName;
-//        this.custIc = custIc;
-//        this.contact = contact;
-//        this.email = email;
-//        this.balance = balance;
-//    }
-//
-//    public String getCustId() {
-//        return custId;
-//    }
-//
-//    public void setCustId(String custId) {
-//        this.custId = custId;
-//    }
-//
-//    public static String getCustName() {
-//        return custName;
-//    }
-//
-//    public void setCustName(String custName) {
-//        this.custName = custName;
-//    }
-//
-//    public int getCustIc() {
-//        return custIc;
-//    }
-//
-//    public void setCustIc(int custIc) {
-//        this.custIc = custIc;
-//    }
-//
-//    public String getContact() {
-//        return contact;
-//    }
-//
-//    public void setContact(String contact) {
-//        this.contact = contact;
-//    }
-//
-//    public String getEmail() {
-//        return email;
-//    }
-//
-//    public void setEmail(String email) {
-//        this.email = email;
-//    }
-//
-//    public static int getBalance() {
-//        return balance;
-//    }
-//
-//    public void setBalance(int balance) {
-//        this.balance = balance;
-//    }
-//}
+import java.util.ArrayList;
+
+public class Customer {
+    private String custId;
+    private static String custName;
+    private int custIc;
+    private String contact;
+    private String email;
+    private double balance;
+    private ArrayList<Reservation> reservation = new ArrayList<>();
+
+    public Customer() {
+    }
+
+    public Customer(String custId, int custIc, String contact, String email, double balance) {
+        this.custId = custId;
+        this.custIc = custIc;
+        this.contact = contact;
+        this.email = email;
+        this.balance = balance;
+        this.reservation = new ArrayList<>();
+    }
+
+    public String getCustId() {
+        return custId;
+    }
+
+    public void setCustId(String custId) {
+        this.custId = custId;
+    }
+
+    public static String getCustName() {
+        return custName;
+    }
+
+    public static void setCustName(String custName) {
+        Customer.custName = custName;
+    }
+
+    public int getCustIc() {
+        return custIc;
+    }
+
+    public void setCustIc(int custIc) {
+        this.custIc = custIc;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public void addReservation(Reservation reservation){
+        this.reservation.add(reservation);
+    }
+
+    public ArrayList<Reservation> getReservation() {
+        return reservation;
+    }
+
+    public void setReservation(ArrayList<Reservation> reservation) {
+        this.reservation = reservation;
+    }
+
+    @Override
+    public String toString() {
+        String reservationDetails = "";
+        for (Reservation r : reservation) {
+            reservationDetails += r.toString() + "\n";
+        }
+
+        return  "\n==========================================\n" +
+                "|          Reservation Summary           |\n" +
+                "==========================================\n" +
+                "Customer ID : " + custId + reservationDetails +
+                "============================================";
+
+    }
+}
+
+
+
