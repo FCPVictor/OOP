1a717dfda8fb78499151e7b9aeb8f3548e69e54a
EbiPrawn <ctgebisan@gmail.com>
2023-05-13 14:03:17 +0800

    Service update

---
+++ OOPAssignment/src/Service/BevMenu.java
@@ -0,0 +1,30 @@
+public class BevMenu extends MenuAbs {
+    private int quantity;
+    public BevMenu(){
+
+    }
+
+    public BevMenu(String name, double price){
+        super(name,price);
+    }
+
+    public BevMenu(String name, double price, int quantity ){
+        super(name,price);
+        this.quantity =quantity;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+
+
+    @Override
+    public double order(int quantity, double price) {
+        return getPrice() * quantity;
+    }
+}
