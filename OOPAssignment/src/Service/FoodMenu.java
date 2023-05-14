d3431a926e481fac51af6b3d1748e26bf4b461a6
EbiPrawn <ctgebisan@gmail.com>
2023-05-14 22:12:53 +0800

    food

--- OOPAssignment/src/Service/FoodMenu.java
+++ OOPAssignment/src/Service/FoodMenu.java
@@ -34,18 +34,7 @@ public class FoodMenu extends Menu {
         this.quantity = quantity;
     }
 
-    public static double printFoodOrders(ArrayList<FoodMenu> foodOrders) {
-        System.out.println("Your food orders:");
-        double subTotal = 0;
-        for (int i = 0; i < foodOrders.size(); i++) {
-            FoodMenu foodOrder = foodOrders.get(i);
-            double foodSubTotal = foodOrder.order(foodOrder.getQuantity(), foodOrder.getPrice());
-            System.out.printf("%-4d%-20s %-2d %-1s RM%.2f %n", (i + 1), foodOrder.getName(), foodOrder.getQuantity(),"x", foodSubTotal);
-            subTotal += foodSubTotal;
-        }
-        System.out.println();
-        return subTotal;
-    }
+
 
     @Override
     public double order(int quantity, double price) {
--- OOPAssignment/src/Service/TransMenu.java
+++ OOPAssignment/src/Service/TransMenu.java
@@ -25,18 +25,7 @@ public class TransMenu extends Menu {
         this.quantity = quantity;
     }
 
-    public static double printTransportOrders(ArrayList<TransMenu> transOrders) {
-        System.out.println("Your transport orders:");
-        double subTotal = 0;
-        for (int i = 0; i < transOrders.size(); i++) {
-            TransMenu transOrder = transOrders.get(i);
-            double transSubTotal = transOrder.order(transOrder.getQuantity(), transOrder.getPrice());
-            System.out.printf("%-4d%-20s %-2d %-1s RM%.2f %n", (i + 1), transOrder.getName(), transOrder.getQuantity(),"x", transSubTotal);
-            subTotal += transSubTotal;
-        }
-        System.out.println();
-        return subTotal;
-    }
+
 
     @Override
     public double order(int quantity, double price) {
